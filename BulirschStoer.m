c022ef78ed255c681e5b9eeb6e8476b1ffa855b1
Matt Kelly <matthew.kelly2@gmail.com>
2016-02-20 13:56:55 -0500

    Cleaned up code, added documentation

--- BulirschStoer.m
+++ BulirschStoer.m
@@ -3,8 +3,8 @@ function [z, info] = BulirschStoer(dynFun,t,z0,tol)
 %
 % Solves an initial value problem using the Bulirsch-Stoer method. This
 % method is ideal for high-accuracy solutions to smooth initial value
-% problems. 
-% 
+% problems.
+%
 % Computes z(t) such that dz/dt = dynFun(t,z), starting from the initial
 % state z0. The solution at the grid-points will be accurate to within tol.
 %
@@ -24,34 +24,65 @@ function [z, info] = BulirschStoer(dynFun,t,z0,tol)
 %
 % OUTPUTS:     (nt = n+1)
 %   z = [nz,nt] = solution to the initial value problem
+%   info
+%       .error = [nx,nt] = error estimate at each grid point
+%       .nFunEval = [1,nt] = number of function evaluations for each point
 %
 % NOTES:
 %   Implementation details:
 %   http://web.mit.edu/ehliu/Public/Spring2006/18.304/implementation_bulirsch_stoer.pdf
 %
+% TUTORIAL:
+%   The big idea here is based on Richardson extrapolation. Suppose that
+%   you solve an initial value problem using the modified mid-point method
+%   (below), where you can pick the number of sub-steps (n). The solution
+%   will become increasingly accurate as the number of sub-steps becomes
+%   large.
+%
+%   Now, imagine that you solve the same problem several times, each time
+%   using more sub-steps. You should be able to observe a trend: the
+%   solutions asymtotically approach some value. Richardson exprapolation
+%   is the math (algorithm) that looks a this sequence of improving
+%   approximations to the solution, and then extrapolates, to the limit as
+%   the step size goes to zero.
+%
+%   The modified mid-point method is not chosen at random. It has a special
+%   property: the expression for the error goes up by powers of two, rather
+%   than one. This makes the convergence particularily good.
+%
+%   This function (BulirschStoer) s just a wrapper for the sub-function
+%   BulirschStoerStep, which is where the real work is done.
+%
 
+% If a step fails, how many sub-steps should be created in its place?
+nStepRefine = 3;  
 
+% Simple logistics and memory allocation:
 nt = length(t);
 nz = size(z0,1);
 z = zeros(nz,nt);
 z(:,1) = z0;
-
 info.error = zeros(size(z));
 info.nFunEval = zeros(1,nt);
+
+% March forward in time, from grid-point to grid-point
 for i=2:nt
+    
     tSpan = [t(i-1), t(i)];
     [zF, stepInfo] = BulirschStoerStep(dynFun, tSpan, z(:,i-1), tol);
+    
     if strcmp(stepInfo.exit,'converged')  %Successful step!
-       z(:,i) = zF; 
-       info.error(:,i) = stepInfo.error;
-       info.nFunEval(i) = stepInfo.nFunEval;
+        z(:,i) = zF;
+        info.error(:,i) = stepInfo.error;
+        info.nFunEval(i) = stepInfo.nFunEval;
+        
     else  %Failed to converge -- try again on a better mesh
-        nGridRefine = 4;  % 
-        time = linspace(tSpan(1),tSpan(2),nGridRefine);
+        time = linspace(tSpan(1),tSpan(2),nStepRefine+1);
         [zTmp, infoTmp] = BulirschStoer(dynFun,time,z(:,i-1),tol);
         z(:,i) = zTmp(:,end);
         info.error(:,i) = infoTmp.error(:,end);
         info.nFunEval(i) = sum(infoTmp.nFunEval);
+        
     end
 end
 
@@ -62,26 +93,52 @@ end
 
 
 function [zF, info] = BulirschStoerStep(dynFun, tSpan, z0, tol)
+% [zF, info] = BulirschStoerStep(dynFun, tSpan, z0, tol)
+%
+% Computes a single step using the Bulirsch-Stoer method
+%
+% INPUTS:
+%   dynFun = function handle for the system dynamics
+%       dz = dynFun(t,z)
+%           t = scalar time
+%           z = [nz,1] = state as column vector
+%           dz = [nz,1] = derivative of state as column vector
+%   tSpan = [1,2] = [t0, tF] = time span for the step
+%   z0 = [nz,1] = initial state vector
+%   tol = [nz,1] = error tolerance along each dimension. If tol is a
+%       scalar, then all dimensions will satisfy that error tolerance.
+%
+% OUTPUTS:     (nt = n+1)
+%   zF = [nz,1] = final state
+%   info = struct with solver information:
+%       .exit = exit condition
+%           'converge' = successful convergence
+%           'maxRefine' = reached max refinement; did not converge
+%       .error = [nz,1] = error estimate along each dimension
+%       .nFunEval = scalar int = count calls to dynFun
+%       .nRefine = how many refinement steps were required?
+%
+% NOTES:
+%   Implementation details:
+%   http://web.mit.edu/ehliu/Public/Spring2006/18.304/implementation_bulirsch_stoer.pdf
+%
 
-nRefineMax = 8;   % How many attempts at refinement before giving up
+% Set an upper limit on the number of mesh refinements in the sequence
+nRefineMax = 8; 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%
-
+% Simple logistics and memory allocation
 n = 2*(1:nRefineMax);
-
 nz = size(z0,1);
-
 if length(tol)==1
     tol = tol*ones(size(z0));
 end
-
 T = zeros(nz,nRefineMax,nRefineMax);   %Extrapolation table
 E = zeros(nz,nRefineMax);   %Error estimate table
 
 info.exit = 'maxRefine';  %Assume that we fail to meet tolerance
-for j=1:nRefineMax
+for j=1:nRefineMax  %Loop over the sequence of improving meshes
     
-    % Compute the current estimate of the solution:
+    % Compute the estimate of the solution on the current mesh
     [~,z] = modifiedMidpointRule(dynFun, tSpan, z0, n(j));
     T(:,j,1) = z(:,end);
     
@@ -127,7 +184,7 @@ function [t,z] = modifiedMidpointRule(dynFun, tSpan, z0, n)
 % [t,z] = modifiedMidpointRule(dynFun, tSpan, z0, n)
 %
 % Approximates the solution to the initial value problem by numerical
-% integration with the modified mid-point rule. 
+% integration with the modified mid-point rule.
 %
 % INPUTS:
 %   dynFun = function handle for the system dynamics
@@ -161,7 +218,7 @@ z = zeros(nz,n);
 z(:,1) = z0;
 z(:,2) = z0 + h*dynFun(t0,z0);
 for i=3:nt
-   z(:,i) = z(:,i-2) + 2*h*dynFun(t(i-1),z(:,i-1)); 
+    z(:,i) = z(:,i-2) + 2*h*dynFun(t(i-1),z(:,i-1));
 end
 
 % Refine the final point using the dynamics function at final point.
--- TEST_bulirschStoer.m
+++ TEST_bulirschStoer.m
@@ -1,36 +1,47 @@
 % TEST - Bulirsch-Stoer Integration method
 %
+% For method details:
+% >> help BulirschStoer
+%
+% OUTLINE:
+%   1) Set up an initial value problem
+%   2) Solve using Bulirsch-Stoer
+%   3) Solve using ode45
+%   4) Compare the solution
+%
 
-tSpan = [0,50];   % Time span
+tSpan = [0,10];   % Time span for initial value problem
 
-z0 = [0.3; 2.5];   %Initial state
+z0 = [0.6; 2.5];   %Initial state
 
 % Dynamical system - driven damped pendulum
 dynFun = @(t,z)( [z(2,:);  cos(t) - 0.1*z(2,:) - sin(z(1,:))] );
 
-% Test the bulirsch-stoer method
-tol = 1e-14;
-t = linspace(tSpan(1), tSpan(2), 20);
+% Solve using Bulirsch-Stoer method
+tol = 1e-12;
+t = linspace(tSpan(1), tSpan(2), 25);
 [z, info] = BulirschStoer(dynFun,t,z0,tol);
 
-% Accurately solve using ode45:
+% Solve using ode45:
 options = odeset('AbsTol',1e-12, 'RelTol',1e-12);
 sol = ode45(dynFun,tSpan,z0,options);
-zSoln = deval(sol,t);
+tSoln = linspace(tSpan(1), tSpan(2), 150);
+zSoln = deval(sol,tSoln);
+zCheck = deval(sol,t);
 
 %%%% Plot!
 
 figure(1); clf;
 
 subplot(2,2,1); hold on;
-plot(t,zSoln(1,:),'k-')
+plot(tSoln,zSoln(1,:),'k-')
 plot(t,z(1,:),'ro','MarkerSize',10,'LineWidth',2);
 legend('ode45','BulirschStoer')
 ylabel('angle')
 title('Solution')
 
 subplot(2,2,3); hold on;
-plot(t,zSoln(2,:),'k-')
+plot(tSoln,zSoln(2,:),'k-')
 plot(t,z(2,:),'ro','MarkerSize',10,'LineWidth',2);
 legend('ode45','BulirschStoer')
 xlabel('time')
@@ -38,13 +49,13 @@ ylabel('rate')
 
 
 subplot(2,2,2); hold on;
-plot(t,abs(z(1,:) - zSoln(1,:)),'ko','MarkerSize',10,'LineWidth',2);
+plot(t,abs(z(1,:) - zCheck(1,:)),'ko','MarkerSize',10,'LineWidth',2);
 ylabel('angle')
-title('Error')
+title('| Ode45 - BulirschStoer |')
 set(gca,'yScale','log')
 
 subplot(2,2,4); hold on;
-plot(t,abs(z(2,:) - zSoln(2,:)),'ko','MarkerSize',10,'LineWidth',2);
+plot(t,abs(z(2,:) - zCheck(2,:)),'ko','MarkerSize',10,'LineWidth',2);
 xlabel('time')
 ylabel('rate')
 set(gca,'yScale','log')
